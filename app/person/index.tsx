cc66b3c0f60569f686eabcc6a7708d6438304d64
12Aleks <tanais2010@yahoo.com>
2025-06-10 13:09:29 +0200

    Styles for errrors have been added

--- app/person/index.tsx
+++ app/person/index.tsx
@@ -57,14 +57,18 @@ export default function Person() {
         <main  className="flex flex-col items-center justify-center p-12">
             <h1 className="text-lg mb-5">People</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row items-center gap-3 justify-center">
-                <input {...register("firstName")} placeholder="First name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                {errors.firstName && <p>{errors.firstName.message}</p>}
-
+                <div className="flex flex-col">
+                    <input {...register("firstName")} placeholder="First name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                    {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
+                </div>
+                <div className="flex flex-col">
                 <input {...register("lastName")} placeholder="Last name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                {errors.lastName && <p>{errors.lastName.message}</p>}
-
+                {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
+                </div>
+                    <div className="flex flex-col">
                 <input type="number" {...register("age")} placeholder="Person Age" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                {errors.age && <p>{errors.age.message}</p>}
+                {errors.age && <p className="text-red-500">{errors.age.message}</p>}
+                    </div>
 
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
             </form>
